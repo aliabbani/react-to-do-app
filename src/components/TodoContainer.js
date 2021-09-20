64600b4774cb0edc8478019803c08f31099c9bd1
ali abbani <aliox@ahlam.com>
2021-09-20 14:15:01 +0300

    Add header and todoItem

--- src/components/TodoContainer.js
+++ src/components/TodoContainer.js
@@ -1,13 +1,34 @@
 import React from "react"
+import TodosList from "./TodosList";
+import Header from "./Header";
 
 class TodoContainer extends React.Component {
-  render() {
-    return (
-      <div>
-        <h1>Hello from Create React App</h1>
-        <p>I am in a React Component!</p>
-      </div>
-    )
-  }
+    state = {
+        todos: [
+          {
+            id: 1,
+            title: "Setup development environment",
+            completed: true
+          },
+          {
+            id: 2,
+            title: "Develop website and add content",
+            completed: false
+          },
+          {
+            id: 3,
+            title: "Deploy to live server",
+            completed: false
+          }
+        ]
+    };
+    render() {
+        return (
+          <div>
+            <Header />
+            <TodosList todos={this.state.todos} />
+          </div>
+        );
+    }
 }
 export default TodoContainer
