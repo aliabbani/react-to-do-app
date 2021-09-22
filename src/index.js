b8f11e541947eeb2801b4b41f541a029ee6b46f4
ali abbani <aliox@ahlam.com>
2021-09-22 08:02:10 +0300

    Fix eslint linter errors

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
-import React from "react"
-import ReactDOM from "react-dom"
-//component file
-import TodoContainer from "./functionBased/components/TodoContainer"
-import "./functionBased/App.css"
-import { BrowserRouter as Router } from "react-router-dom"
+import React from 'react';
+import ReactDOM from 'react-dom';
+// component file
+import { BrowserRouter as Router } from 'react-router-dom';
+import TodoContainer from './functionBased/components/TodoContainer';
+import './functionBased/App.css';
 
 ReactDOM.render(
-    <React.StrictMode>
-      <Router>
-        <TodoContainer />
-      </Router>
-    </React.StrictMode>,
-    document.getElementById("root")
-)
+  <React.StrictMode>
+    <Router>
+      <TodoContainer />
+    </Router>
+  </React.StrictMode>,
+  document.getElementById('root'),
+);
